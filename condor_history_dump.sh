67be055471853171e33cc34e13490b6035c8e9dc
JAMES LETTS <jletts@ucsd.edu>
2014-04-30 09:40:30 -0700

    Updated.

--- condor_history_dump.sh
+++ condor_history_dump.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 POOLNAME="glidein-collector-2.t2.ucsd.edu"
 
-OUTFILE=/crabprod/CSstoragePath/Monitor/monitor-anaops-history-`/bin/date +%F-Z%R -u`.txt
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/monitor-anaops-history-`/bin/date +%F-Z%R -u`.txt
 condor_history_dump $POOLNAME > ${OUTFILE}.tmp
 mv ${OUTFILE}.tmp $OUTFILE
 
-OUTFILE=/crabprod/CSstoragePath/Monitor/latest-history.txt
-/home/letts/scripts/condor_history_analyze.sh $POOLNAME > ${OUTFILE}.tmp
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/latest-history.txt
+$glideinWMSMonitor_RELEASE_DIR/condor_history_analyze.sh $POOLNAME > ${OUTFILE}.tmp
 mv ${OUTFILE}.tmp $OUTFILE
 
 exit
