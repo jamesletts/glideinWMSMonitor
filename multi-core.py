57b1ac838ccebe997e941bdfb7a6e76e961ae7ca
JAMES LETTS <jletts@ucsd.edu>
2015-03-27 18:30:36 -0700

    Updated for the new format

--- multi-core.py
+++ multi-core.py
@@ -4,27 +4,21 @@ import glob
 import json
 import sys
 
-print "0UNIX time,Date,Time,Pool Name,",
+print "0Site,UNIX time,Date,Time,",
 
 print "All,",
 print "Static,",
-print "Static Retiring,",
-print "Dynamic+Partitionable,",
-print "Dynamic+Partitionable Retiring,",
+print "Multi-core,",
 
 print "All Idle,",
 print "Static Idle,",
-print "Static Retiring Idle,",
-print "Dynamic+Partitionable Idle,",
-print "Dynamic+Partitionable Retiring Idle,",
+print "Multi-core Idle,",
 
 print "All Idle %,",
 print "Static Idle %,",
-print "Static Retiring Idle %",
-print "Dynamic+Partitionable Idle %,",
-print "Dynamic+Partitionable Retiring Idle%"
+print "Multi-core Idle %,"
 
-FILES=glob.glob("/crabprod/CSstoragePath/Monitor/json/monitor-T1_ES_PIC-*.json")
+FILES=glob.glob("/crabprod/CSstoragePath/Monitor/json/monitor-*.json")
 for FILE in FILES :
 
   try :
@@ -34,11 +28,15 @@ for FILE in FILES :
   except ValueError :
     continue
 
+  SITE=FILE.split('-')[1]
+  print (SITE+",").rjust(14),
+
   TABLES=d['Multi-core pilot monitoring']
 
   now=int(d['Multi-core pilot monitoring']['Time'])
   readable_date=datetime.datetime.utcfromtimestamp(now).strftime('%Y-%m-%d, %H:%M:%S,')
   print str(now)+",",readable_date,
+
 
   totals={}
   wasted={}
@@ -53,28 +51,20 @@ for FILE in FILES :
       wasted[TABLE]=float(total-busy)
 
   total_totals=float(totals['Total glidein Cpus'])
-  static_totals=float(totals['Static multi-core glidein Cpus'])
-  static_retiring_totals=float(totals['Static multi-core retiring glidein Cpus'])
+  static_totals=float(totals['Static glidein Cpus'])
   partitionable_totals=float(totals['Dynamic glidein Cpus']+totals['Partitionable glidein Cpus'])
-  partitionable_retiring_totals=float(totals['Dynamic retiring glidein Cpus']+totals['Partitionable retiring glidein Cpus'])
 
   print '{0:6.0f},'.format(total_totals),
   print '{0:6.0f},'.format(static_totals),
-  print '{0:6.0f},'.format(static_retiring_totals),
   print '{0:6.0f},'.format(partitionable_totals),
-  print '{0:6.0f},'.format(partitionable_retiring_totals),
 
   total_wasted=float(wasted['Total glidein Cpus'])
-  static_wasted=float(wasted['Static multi-core glidein Cpus'])
-  static_retiring_wasted=float(wasted['Static multi-core retiring glidein Cpus'])
+  static_wasted=float(wasted['Static glidein Cpus'])
   partitionable_wasted=float(wasted['Dynamic glidein Cpus']+wasted['Partitionable glidein Cpus'])
-  partitionable_retiring_wasted=float(wasted['Dynamic retiring glidein Cpus']+wasted['Partitionable retiring glidein Cpus'])
 
   print '{0:6.0f},'.format(total_wasted),
   print '{0:6.0f},'.format(static_wasted),
-  print '{0:6.0f},'.format(static_retiring_wasted),
   print '{0:6.0f},'.format(partitionable_wasted),
-  print '{0:6.0f},'.format(partitionable_retiring_wasted),
 
   try :
     total_wasted/=totals['Total glidein Cpus']
@@ -82,29 +72,17 @@ for FILE in FILES :
     total_wasted=0.
 
   try :
-    static_wasted/=totals['Static multi-core glidein Cpus']
+    static_wasted/=totals['Static glidein Cpus']
   except ZeroDivisionError :
     static_wasted=0.
   
-  try :
-    static_retiring_wasted/=totals['Static multi-core glidein Cpus']
-  except ZeroDivisionError :
-    static_retiring_wasted=0.
-
   try :
     partitionable_wasted/=(totals['Dynamic glidein Cpus']+totals['Partitionable glidein Cpus'])
   except ZeroDivisionError :
     partitionable_wasted=0.
 
-  try :
-    partitionable_retiring_wasted/=(totals['Dynamic glidein Cpus']+totals['Partitionable glidein Cpus'])
-  except ZeroDivisionError :
-    partitionable_retiring_wasted=0.
-
   print '{0:5.1f}%,'.format(100.0*total_wasted),
   print '{0:5.1f}%,'.format(100.0*static_wasted),
-  print '{0:5.1f}%,'.format(100.0*static_retiring_wasted),
-  print '{0:5.1f}%,'.format(100.0*partitionable_wasted),
-  print '{0:5.1f}%'.format(100.0*partitionable_retiring_wasted)
+  print '{0:5.1f}%,'.format(100.0*partitionable_wasted)
 
 sys.exit()
