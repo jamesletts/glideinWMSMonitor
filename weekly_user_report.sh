c6bc6af95de48413cc64dba27675f119a25c21a1
jamesletts <jletts@ucsd.edu>
2018-01-15 19:30:21 -0800

    Updated for January 2018 report

--- weekly_user_report.sh
+++ weekly_user_report.sh
@@ -5,21 +5,21 @@ if [ -z $glideinWMSMonitor_OUTPUT_DIR ] ; then
 else
   source $glideinWMSMonitor_RELEASE_DIR/bashrc
 fi
-OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/weekly-report-user-`/bin/date +%Y-%m-%d`.txt
-#dashboard_user_report 7 30 > $OUTFILE
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/weekly-report-user-2017.txt
+dashboard_user_report 7 56 > $OUTFILE
 
-OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/monthly-report-user-2014.txt
-dashboard_user_report 31 1 2014-02-01            > $OUTFILE
-dashboard_user_report 28 1 2014-03-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2014-04-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2014-05-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2014-06-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2014-07-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2014-08-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2014-09-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2014-10-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2014-11-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2014-12-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2015-01-01 | tail -1 >> $OUTFILE
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/monthly-report-user-2017.txt
+dashboard_user_report 31 1 2017-02-01            > $OUTFILE
+dashboard_user_report 28 1 2017-03-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2017-04-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2017-05-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2017-06-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2017-07-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2017-08-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2017-09-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2017-10-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2017-11-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2017-12-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-01-01 | tail -1 >> $OUTFILE
 
 exit
