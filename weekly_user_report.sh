0b1d71bd4468cec1b3281fde1349d8f32d8c9a7b
jamesletts <jletts@ucsd.edu>
2019-01-16 16:20:22 -0800

    Updated for 2018.

--- weekly_user_report.sh
+++ weekly_user_report.sh
@@ -5,21 +5,21 @@ if [ -z $glideinWMSMonitor_OUTPUT_DIR ] ; then
 else
   source $glideinWMSMonitor_RELEASE_DIR/bashrc
 fi
-OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/weekly-report-user-2017.txt
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/weekly-report-user-2018.txt
 dashboard_user_report 7 56 > $OUTFILE
 
-OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/monthly-report-user-2017.txt
-dashboard_user_report 31 1 2017-02-01            > $OUTFILE
-dashboard_user_report 28 1 2017-03-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2017-04-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2017-05-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2017-06-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2017-07-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2017-08-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2017-09-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2017-10-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2017-11-01 | tail -1 >> $OUTFILE
-dashboard_user_report 30 1 2017-12-01 | tail -1 >> $OUTFILE
-dashboard_user_report 31 1 2018-01-01 | tail -1 >> $OUTFILE
+OUTFILE=$glideinWMSMonitor_OUTPUT_DIR/monthly-report-user-2018.txt
+dashboard_user_report 31 1 2018-02-01            > $OUTFILE
+dashboard_user_report 28 1 2018-03-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-04-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2018-05-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-06-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2018-07-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-08-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-09-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2018-10-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2018-11-01 | tail -1 >> $OUTFILE
+dashboard_user_report 30 1 2018-12-01 | tail -1 >> $OUTFILE
+dashboard_user_report 31 1 2019-01-01 | tail -1 >> $OUTFILE
 
 exit
